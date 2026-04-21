9c4402b3562f705036537c25959e414215e7c871
HoangDo <donguyen@gmail.com>
2026-04-21 22:58:49 +0700

    add file .env

--- BE/tiem-ve-climping-rose-be/src/main/java/vn/edu/iuh/fit/climpingrose/dtos/dtos/CouponDTO.java
+++ BE/tiem-ve-climping-rose-be/src/main/java/vn/edu/iuh/fit/climpingrose/dtos/dtos/CouponDTO.java
@@ -12,16 +12,13 @@ import java.util.Date;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CouponDTO extends BaseResponse {
+    String couponId;
     String code;
     String imageUrl;
-
     String description;
     String condition;
     BigDecimal discountPercentage;
-
     Instant startDate;
     Instant endDate;
-
     Boolean isPublic;
-
 }
